afb30ecca538ebbec748a2ffe6088e974a2f4cbd
Anatoliy Tereshenko <anatolio081@gmail.com>
2019-10-14 20:12:13 +0300

    111

--- parser2.py
+++ parser2.py
@@ -1,70 +1,124 @@
 import argparse
+import os
+import sys
 import re
 import json
 
-# arg_parser = argparse.ArgumentParser(description="access.log parser")
-# file_name = arg_parser.add_argument('-f', '--file', dest="filename", help="enter path to file", metavar="FILE")
-# args = arg_parser.parse_args()
-# file_path = args.filename
-# dict_ip = {}
-output = {}
-file_path = "access.log"
+arg_parser = argparse.ArgumentParser(description="access.log parser")
+file_name_arg = arg_parser.add_argument('-f', '--file', dest="filename", help="enter path to file", metavar="FILE")
+file_dir_arg = arg_parser.add_argument('-d', '--directory', dest="directory", help="enter path to files", metavar="DIR")
+output={}
 
-with open(file_path) as file:
-    requests = re.findall(r"\] \"(POST|GET|PUT|DELETE|HEAD)", file.read())
-    output["count_all_requests"] = requests.__len__()
-    output["count_requests_by_method"] = {
-        "GET": requests.count("GET"),
-        "POST": requests.count("POST"),
-        "PUT": requests.count("PUT"),
-        "DELETE": requests.count("DELETE"),
-        "HEAD": requests.count("HEAD")
-    }
+args = arg_parser.parse_args()
+file_name = args.filename
+#file_dir = args.directory
+file_dir = "acc_dir"
+if not file_name == None:
+    file_path = file_name
+elif not file_dir == None:
+    pass
+else:
+    print("Error!!! file/directory is not found")
+    sys.exit()
 
-with open(file_path) as file:
-    ip_count = {}
-    ip = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", file.read())
-    for i in ip:
-        if i in ip_count.keys():
-            ip_count[i] += 1
-        else:
-            ip_count[i] = 0
-    list_d = list(ip_count.items())
-    list_d.sort(key=lambda i: i[1])
-    list_d.reverse()
-    top_ip={}
-    for l in range(0, 10):
-         top_ip[list_d[l][0]] = list_d[l][1]
-    output["top_request_by_ip"] = top_ip
 
-print(json.dumps(output))
-
-# print(req)
-# print("POST=" + str(POST_ct))
-# print("GET=" + str(GET_ct))
-# print("PUT=" + str(PUT_ct))
-# print("DELETE=" + str(DELETE_ct))
-# print("HEAD=" + str(HEAD_ct))
+files = os.listdir(file_dir)
+for name in files:
+    print(name)
 
 # with open(file_path) as file:
-#     for index, line in enumerate(file.readlines()):
-#         ip = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line).group()
-#         method = re.search(r"\] \"(POST|GET|PUT|DELETE|HEAD)", line).groups()
-#         ip_req[index] = {ip: method}
-#     for i in range(0, ct_reqests):
-#         print(ip_req[i])
-# for index, line in enumerate(file.readlines()):
-#     ip = re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line).group()
-#     method = re.search(r"\] \"(POST|GET|PUT|DELETE|HEAD)", line).groups()[0]
-#     if dict_ip.get(ip, None) is None:
-#         dict_ip[ip] = {
-#             "GET": 0,
-#             "POST": 0,
-#             "PUT": 0,
-#             "DELETE": 0,
-#             "HEAD": 0,
+#     a = 0
+#     for line in file:
+#         print(str(a))
+#         a += 1
+
+
+
+
+# with open(file_path) as file:
+#     requests = re.findall(r"\] \"(POST|GET|PUT|DELETE|HEAD)", file.read())
+#     output["count_all_requests"] = requests.__len__()
+#     output["count_requests_by_method"] = {
+#         "GET": requests.count("GET"),
+#         "POST": requests.count("POST"),
+#         "PUT": requests.count("PUT"),
+#         "DELETE": requests.count("DELETE"),
+#         "HEAD": requests.count("HEAD")
+#     }
+
+#
+# with open(file_path) as file:
+#     ip_count = {}
+#     ip = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", file.read())
+#     for i in ip:
+#         if i in ip_count.keys():
+#             ip_count[i] += 1
+#         else:
+#             ip_count[i] = 0
+#     list_d = list(ip_count.items())
+#     list_d.sort(key=lambda i: i[1])
+#     list_d.reverse()
+#     top_ip={}
+#     for l in range(0, 10):
+#          top_ip[list_d[l][0]] = list_d[l][1]
+#     output["top_request_by_ip"] = top_ip
+#
+#
+#
+#
+# db_dict = {}
+# id = []
+# method = []
+# url = []
+# ip = []
+# code_cl_error = []
+# with open(file_path) as file:
+#     for line in file:
+#         code = re.findall(r"\s4\d{2}\s", line)
+#         if not code == []:
+#             method.append(re.findall(r"\] \"(POST|GET|PUT|DELETE|HEAD)", line))
+#             url.append(re.findall("https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+", line))
+#             ip.append(re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line))
+#             code_cl_error.append(code)
+# try:
+#     for i in range(0, 10):
+#         db_dict[i] = {
+#             "ip": ip[i][0],
+#             "url": url[i],
+#             "method": method[i][0],
+#             "code": code_cl_error[i][0]
 #         }
-#     dict_ip[ip][method] += 1
-# print(json.dumps(dict_ip, indent=4))
-# with open('output.txt','w') as f:
-#     json.dump(dict_ip, f)
+# except IndexError:
+#     pass
+# output["client_error_top"] = db_dict
+#
+#
+#
+# db_dict = {}
+# id = []
+# method = []
+# url = []
+# ip = []
+# code_cl_error = []
+# with open(file_path) as file:
+#     for line in file:
+#         code = re.findall(r"\s5\d{2}\s", line)
+#         if not code == []:
+#             method.append(re.findall(r"\] \"(POST|GET|PUT|DELETE|HEAD)", line))
+#             url.append(re.findall("https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+", line))
+#             ip.append(re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line))
+#             code_cl_error.append(code)
+# try:
+#     for i in range(0, 10):
+#         db_dict[i] = {
+#             "ip": ip[i][0],
+#             "url": url[i],
+#             "method": method[i][0],
+#             "code": code_cl_error[i][0]
+#         }
+# except IndexError:
+#     pass
+# output["server_error_top"] = db_dict
+#
+# with open('output.txt', 'w') as outfile:
+#     json.dump(output, outfile)
